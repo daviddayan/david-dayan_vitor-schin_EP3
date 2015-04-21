e0609a48e5bfadd514f348010c6c2f75d229f6c4
daviddayan <david.dd.dayan@gmail.com>
2015-04-21 20:45:33 -0300

    pronto 2
    pequena alteraçao no final

--- Projeto3.py
+++ Projeto3.py
@@ -313,7 +313,7 @@ plt.legend(loc="upper right")
 plt.plot(num_dias,cal_dia,"b",label="calorias ideias")
 plt.legend(loc="upper right")
 plt.plot(num_dias,cal_dia,"p")
-plt.axis(0,num_dias[6],0,consumo_total)
+plt.axis(0,num_dias[6],0,cal_dia)
 plt.show()
 
 
